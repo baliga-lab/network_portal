dae8fbad39bf91e957cd530ab2f4c7ebb2399cd5
Christopher Bare <cbare@como.systemsbiology.net>
2012-06-25 15:07:33 -0700

    minor changes for deploy on como

--- web_app/settings.py
+++ web_app/settings.py
@@ -14,8 +14,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'network_portal',        # Or path to database file if using sqlite3.
-        'USER': 'dj_ango',               # Not used with sqlite3.
-        'PASSWORD': 'django',            # Not used with sqlite3.
+        'USER': '',               # Not used with sqlite3.
+        'PASSWORD': '',            # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
