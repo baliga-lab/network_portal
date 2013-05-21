cf75aaa6be409fae95b30a02be0af07201591fbf
Wei-ju Wu <weiju.wu@gmail.com>
2013-05-21 13:13:54 -0700

    fixed windows config

--- web_app/nwportal/settings/devwin.py
+++ web_app/nwportal/settings/devwin.py
@@ -6,6 +6,5 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__), 'static').replace('\\','/'),
-    # os.path.join(os.path.dirname(__file__), '../public').replace('\\','/'),
+    root('static'),
 )
