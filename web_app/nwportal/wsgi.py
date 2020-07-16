4f35cc8f79a2fa297bcdeff550f78aaf01a95a5d
Wei-ju Wu <weiju.wu@gmail.com>
2020-07-16 12:03:40 -0700

    updated server certificates

--- web_app/nwportal/wsgi.py
+++ web_app/nwportal/wsgi.py
@@ -1,5 +1,8 @@
 import os
+import httplib2
 
+
+h = httplib2.Http(ca_certs='/local/apache-stuff/cacert.pem')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nwportal.settings.production")
 os.environ['MPLCONFIGDIR'] = '/tmp'
 from django.core.wsgi import get_wsgi_application
